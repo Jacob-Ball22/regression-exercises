a64bfd7cabec2351d241e4165cbc38481d34608d
Jacob-Ball22 <jeball22@gmail.com>
2023-12-12 16:32:55 -0600

    Updated split function

--- wrangle.py
+++ wrangle.py
@@ -45,7 +45,7 @@ def wrangle_zillow():
     
     return df
     
-def split_data(df, col):
+def split_data(df):
     """
     Recieves dataframe as 'df' and target variable to stratify as 'col'
     First split does a 60% train and 40% validate
@@ -55,14 +55,12 @@ def split_data(df, col):
     train, validate_test = train_test_split(df, #send in initial df
                 train_size = 0.60, #size of the train df, and the test size will default to 1-train_size
                 random_state = 123, #set any number here for consistency
-                stratify = df[col] #we should stratify on our target variable
                 )
     
     #second split
     validate, test = train_test_split(validate_test, #we are spliting the 40% df we just made
                 test_size = 0.50, #split 50/50
                 random_state = 123, #gotta send in a random seed
-                stratify = validate_test[col] #still got to stratify
                 )
     
     return train, validate, test
